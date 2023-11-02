fbe7f439a7f590841c75d48b89399929531011f9
Johannes Fischer <johannes@oneswitchboard.com>
2023-11-02 13:08:14 -0400

    Looping dictionary

--- data_scraper.py
+++ data_scraper.py
@@ -13,39 +13,31 @@ def convert_non_numeric_to_string(df):
 
 # Function to fetch data and store in Postgres
 def fetch_and_store_data():
-
     # Establish connection to the Postgres database
     engine = create_engine(os.getenv("db_connection_url"))
-    
-    response = requests.get("https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/station_status.json")
-    data = response.json()
-    df = pd.DataFrame(data['data']['stations'])
 
-    # Add a column for the current timestamp
-    df['data_retrieved'] = time.time()
-    df = convert_non_numeric_to_string(df)
+    # Set the time data is being retrieved
+    data_retrieved = time.time()
 
-    df.to_sql('station_status', engine, if_exists='append', index=False)
+    data_urls = {
+        "station_status": "https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/station_status.json",
+        "station_info": "https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/station_information.json",
+        "bike_status": "https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/free_bike_status.json"
+    }
 
-    response = requests.get("https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/station_information.json")
-    data = response.json()
-    df = pd.DataFrame(data['data']['stations'])
-    
-    # Add a column for the current timestamp
-    df['data_retrieved'] = time.time()
-    df = convert_non_numeric_to_string(df)
+    for table_name, url in data_urls.items():
+        response = requests.get(url)
+        data = response.json()
+        if table_name == 'bike_status':
+            df = pd.DataFrame(data['data']['bikes'])
+        else:
+            df = pd.DataFrame(data['data']['stations'])
 
-    df.to_sql('station_info', engine, if_exists='append', index=False)
+        # Add a column for the current timestamp
+        df['data_retrieved'] = data_retrieved
+        df = convert_non_numeric_to_string(df)
 
-    response = requests.get("https://gbfs.lyft.com/gbfs/2.3/dca-cabi/en/free_bike_status.json")
-    data = response.json()
-    df = pd.DataFrame(data['data']['bikes'])
-
-    # Add a column for the current timestamp
-    df['data_retrieved'] = time.time()
-    df = convert_non_numeric_to_string(df)
-
-    df.to_sql('bike_status', engine, if_exists='append', index=False)
+        df.to_sql(table_name, engine, if_exists='append', index=False)
 
     # Close the database connection
     engine.dispose()
